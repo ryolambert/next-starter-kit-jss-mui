eb846cd7088fa523096857f49ac393c882766efd
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-12-09 12:21:09 +0100

    :wrench: Register addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,20 @@
-import { configure } from '@storybook/react'
+import { addDecorator, configure } from '@storybook/react'
+import { withOptions } from '@storybook/addon-options'
+import { withKnobs } from '@storybook/addon-knobs'
 
 const components = require.context('../components', true, /\/stories\.js$/)
 
 function loadStories() {
   components.keys().forEach(filename => components(filename))
 }
+
+addDecorator(
+  withOptions({
+    name: 'NextJS Starter JSS Kit',
+    sortStoriesByKind: true,
+  }),
+)
+
+addDecorator(withKnobs)
 
 configure(loadStories, module)
