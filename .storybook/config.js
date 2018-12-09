7f4c53198edb65b9894b4a37af0ff2d5cbb033e0
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-12-09 18:12:32 +0100

    :wrench: Add Material Theme Provider and CSS Baseline for default styling

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,11 +2,17 @@ import '../css/global.css'
 import { addDecorator, configure } from '@storybook/react'
 import { withOptions } from '@storybook/addon-options'
 import { withKnobs } from '@storybook/addon-knobs'
+import React from 'react'
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import createCustomTheme from '../components/styles/createCustomTheme'
 
 const components = require.context('../components', true, /\/stories\.js$/)
+const containers = require.context('../containers', true, /\/stories\.js$/)
 
 function loadStories() {
   components.keys().forEach(filename => components(filename))
+  containers.keys().forEach(filename => containers(filename))
 }
 
 addDecorator(
@@ -15,6 +21,16 @@ addDecorator(
     sortStoriesByKind: true,
   }),
 )
+
+addDecorator(story => {
+  const content = story()
+  return (
+    <MuiThemeProvider theme={createCustomTheme()}>
+      <CssBaseline />
+      {content}
+    </MuiThemeProvider>
+  )
+})
 
 addDecorator(withKnobs)
 
